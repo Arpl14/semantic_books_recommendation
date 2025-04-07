6b122498e3359103dfa07b750d294d6ffda4e272
Arpita Lonakadi <104141167+Arpl14@users.noreply.github.com>
2025-04-06 21:19:20 -0400

    Update sitecustomize.py

--- sitecustomize.py
+++ sitecustomize.py
@@ -1,6 +1,9 @@
 import sys
-import pysqlite3
+import importlib
 
-# Override built-in sqlite3 with pysqlite3
-sys.modules["sqlite3"] = pysqlite3
-sys.modules["sqlite"] = pysqlite3.dbapi2
+try:
+    import pysqlite3
+    sys.modules["sqlite3"] = pysqlite3
+    sys.modules["sqlite"] = pysqlite3
+except ImportError:
+    pass
